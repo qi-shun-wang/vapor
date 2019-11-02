9ba41d3c869c545020cd7cc58b1aeaf62a3fdb5a
Shun <shunwang@jetfuel.com.tw>
2019-11-02 21:20:24 +0800

    remove specific platforms.

--- Package.swift
+++ Package.swift
@@ -3,9 +3,6 @@ import PackageDescription
 
 let package = Package(
     name: "vapor",
-    platforms: [
-       .macOS(.v10_14)
-    ],
     products: [
         .library(name: "Vapor", targets: ["Vapor"]),
         .library(name: "XCTVapor", targets: ["XCTVapor"])
